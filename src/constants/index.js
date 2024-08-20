c213e5e2ecb44f0d15ea8a18c4cec4ff6b621146
pauldavis-pro <professional.pauldavis@gmail.com>
2024-08-20 18:44:04 +0500

    feat: realtime-stream-monitoring

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 const constants = Object.freeze({
 	RECENT_CHANGE_URI: "https://stream.wikimedia.org/v2/stream/recentchange",
-	THROTTLING_TIME_IN_MS: 2000, // 3s
+	THROTTLING_TIME_IN_MS: 2000, // 2s
 	DATA_SLICE_LIMIT: 100,
 });
 
